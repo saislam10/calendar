3599d645763ae51b662e1d1b6aa7f26cea625a94
Safwan Islam <saislam@davidson.edu>
2022-05-31 16:49:30 -0500

    fixed the color states and everything else, ready to do local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,35 @@
 var containerEl = $(".container");
 var input = $("#input");
 
-for (var i = 9; i <= 17; i++){
+for (var i = 9; i <= 17; i++) {
     var rows = $("<div class=row>");
     var saveBtn = $("<button>").attr('class', 'col-1 saveBtn');
     var text = $("<textarea>").attr('class', 'col-10')
     var time = $("<div>").attr('class', 'col-1 hour d-flex align-items-center justify-content-center');
     saveBtn.text('Save');
-    if(i<12){
-    time.text(i + "AM");
+    if (i < 12) {
+        time.text(i + "AM");
     }
-    else if(i===12){
-    time.text(i + "PM");
+    else if (i === 12) {
+        time.text(i + "PM");
     }
-    else{
-    time.text(-12+ i + "PM")
+    else {
+        time.text(-12 + i + "PM")
     }
     rows.append(time)
     rows.append(text);
     rows.append(saveBtn);
     containerEl.append(rows);
+
+    if (moment().hour() > i){
+        text.attr('class', 'past col-10')
+    }
+    if (moment().hour() === i){
+        text.attr('class', 'present col-10')
+    }
+    if (moment().hour() < i){
+        text.attr('class', 'future col-10')
+    }
 }
 
 
@@ -38,7 +48,7 @@ for (var i = 9; i <= 17; i++){
 
 //     localStorage.setItem("reminders", JSON.stringify(reminders));
 
-   
+
 
 //     input.val('');
 // };
@@ -53,4 +63,5 @@ for (var i = 9; i <= 17; i++){
 
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+
 
