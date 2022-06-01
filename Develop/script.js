d3378f3ba5418d266132cdf0b36e8f43911d7eef
Safwan Islam <saislam@davidson.edu>
2022-05-31 23:15:26 -0500

    tried to fix local storage, was not successful

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,5 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
-
 for (var i = 9; i <= 17; i++) {
     var containerEl = $(".container");
     var rows = $("<div class=row>");
@@ -8,7 +7,6 @@ for (var i = 9; i <= 17; i++) {
     var text = $("<textarea>").attr('class', 'col-10 text')
     var time = $("<div>").attr('class', 'col-1 hour d-flex align-items-center justify-content-center');
     saveBtn.text('Save');
-
     if (i < 12) {
         time.text(i + "AM");
     }
@@ -18,29 +16,27 @@ for (var i = 9; i <= 17; i++) {
     else {
         time.text(-12 + i + "PM")
     }
-
     rows.append(time)
     rows.append(text);
     rows.append(saveBtn);
     containerEl.append(rows);
-
-    if (moment().hour() > i){
+    if (moment().hour() > i) {
         text.attr('class', 'past col-10')
     }
-    if (moment().hour() === i){
+    if (moment().hour() === i) {
         text.attr('class', 'present col-10')
     }
-    if (moment().hour() < i){
+    if (moment().hour() < i) {
         text.attr('class', 'future col-10')
     }
-
     $(i).children().eq(1).val(localStorage.getItem(i));
 
-}
 
-$("button").on("click", function(event) {
-    localStorage.setItem($(event.target).parent().attr("class"), $(event.target).parent().children().eq(1).val());
+}
+$('button').on("click", function (event) {
+    localStorage.setItem($(event.target).parent().attr("saveBtn"), $(event.target).parent().children().eq(1).val());
 });
+
 
 
 
